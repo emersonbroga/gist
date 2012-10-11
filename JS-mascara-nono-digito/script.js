b8476279c2f88b26a3c9d895843bc3ac479151fa
Gabriel Mateus <gabriel.mateus@boltbrasil.com>
2012-10-11 18:56:47 -0300

    Muda o nome da pasta

---
+++ JS-mascara-nono-digito/script.js
@@ -0,0 +1,22 @@
+/*
+*     ~Le Script
+*/
+
+(function($) {
+    $('#phone').on({
+        'focus': function() {
+            $(this).unmask().mask('(99) 99999999?9');
+        },
+        'focusout': function() {
+            var $element = $(this),
+                value = $element.val(),
+                mask = '(99) 9999-9999?9';
+
+            if(/^\((1{2})\) 9(5[0-9]|6[0-9]|7[01234569]|8[0-9]|9[0-9])/g.test(value)){
+                mask = '(99) 99999-999?9';
+            }
+
+            $element.unmask().mask(mask);
+        }
+    });
+})(jQuery);
